181f068129afcbcd54eaf7a4b33bd1503d5639cf
starjsenthilarrasie <starjsenthilarrasie@gmail.com>
2026-08-26 20:19:32 +0530

    chore: configure app foundation, typography, and core dependencies

---
+++ src/ui/theme.ts
@@ -0,0 +1,15 @@
+export const Theme = {
+  colors: {
+    primary: '#5E56E7',
+    primaryLight: '#F8F7FF',
+    background: '#FFFFFF',
+    greyLight: '#F0F0F6',
+    greyMedium: '#A0A0A0',
+    greyDark: '#333333',
+  },
+  fonts: {
+    regular: 'Montserrat-Regular',
+    semiBold: 'Montserrat-SemiBold',
+    medium: 'Montserrat-Regular',
+  },
+};
